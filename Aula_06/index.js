f437730a98f8af25e186fca97a0abc6d3198dd0b
Leo Ruiz <ruizruizleo@hotmail.com>
2022-03-14 20:53:27 -0300

    primeira metade

---
+++ Aula_06/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.post("/", function (req, res) {
+    res.send("Hello World via post");
+});
+
+app.get("/pagina", function (req, res) {
+    res.send("pagina 2");
+});
+
+app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
