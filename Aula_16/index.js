8f79582c09842cc051ed753a0f81a1a7403c17c8
Leo Ruiz <ruizruizleo@hotmail.com>
2022-03-30 20:37:46 -0300

    CRUD COMPLETO

--- Aula_16/index.js
+++ Aula_16/index.js
@@ -27,4 +27,100 @@ app.get("/filmes/:id", async (req, res) => {
     });
 });
 
+app.get("/criar", (req, res) => {
+  res.render("criar",{mensagem: ""});
+});
+
+app.post("/criar", async (req, res) => {
+  const { nome, descricao, imagem } = req.body;
+  
+  if (!nome) {
+    res.render("criar", {
+      mensagem: "Nome é obrigatório",
+    });
+  }
+
+  if (!imagem) {
+    res.render("criar", {
+      mensagem: "Imagem é obrigatório",
+    });
+  }
+
+  try {
+    const filme = await Filme.create({
+      nome,
+      descricao,
+      imagem,
+    });
+
+    res.render("criar", {
+      filme,
+      mensagem: ""
+    });
+  } catch (err) {
+    console.log(err);
+
+    res.render("criar", {
+      mensagem: "Ocorreu um erro ao cadastrar o Filme!",
+    });
+  }
+});
+
+app.get("/editar/:id", async (req, res) => {
+  const filme = await Filme.findByPk(req.params.id);
+
+  if (!filme) {
+    res.render("editar", {
+      mensagem: "Filme não encontrado!",
+    });
+  }
+
+  res.render("editar", {
+    filme, mensagem: ""
+  });
+});
+
+app.post("/editar/:id", async (req, res) => {
+  const filme = await Filme.findByPk(req.params.id);
+
+  const { nome, descricao, imagem } = req.body;
+
+  filme.nome = nome;
+  filme.descricao = descricao;
+  filme.imagem = imagem;
+
+  const filmeEditado = await filme.save();
+
+  res.render("editar", {
+    filme: filmeEditado,
+    mensagem: "Filme editado com sucesso!",
+  });
+});
+
+app.get("/deletar/:id", async (req, res) => {
+  const filme = await Filme.findByPk(req.params.id);
+
+  if (!filme) {
+    res.render("deletar", {
+      mensagem: "Filme não encontrado!",
+    });
+  }
+
+  res.render("deletar", {
+    filme,mensagem: ""
+  });
+});
+
+app.post("/deletar/:id", async (req, res) => {
+  const filme = await Filme.findByPk(req.params.id);
+
+  if (!filme) {
+    res.render("deletar", {
+      mensagem: "Filme não encontrado!",
+    });
+  }
+
+  await filme.destroy();
+  res.redirect("/");
+});
 app.listen(port || process.env.PORT, () => console.log(`Servidor rodando em http://localhost:${port}`));
